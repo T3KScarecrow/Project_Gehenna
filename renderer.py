687a30aa2bc000ec4078d8c75875975bc0350794
T3KScarecrow <166108092+T3KScarecrow@users.noreply.github.com>
2026-04-02 20:54:30 +0800

    LoadingDots function

--- renderer.py
+++ renderer.py
@@ -8,6 +8,24 @@ def Typer(text:type=str, sleep=0.05):
         time.sleep(sleep)
     time.sleep(0.35)
 
+def LoadingDots():
+    text = "..."
+    Typer(text, 0.25)
+    for i in range(3):
+        for e in range(3):
+            sys.stdout.write("\b")
+            sys.stdout.flush()
+        time.sleep(0.175)
+        sys.stdout.write('.')
+        sys.stdout.flush()
+        time.sleep(0.25)
+        sys.stdout.write('.')
+        sys.stdout.flush()
+        time.sleep(0.25)
+        sys.stdout.write('.')
+        sys.stdout.flush()
+        time.sleep(0.3)
+
 def LoopTyper(loops:type=int, val:type=int, sleep=0.01):
     bar_width = 100
     cube = '■'
