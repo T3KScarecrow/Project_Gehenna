fafe02dfd39984887a871afd14c97fc40fd1c2eb
T3KScarecrow <166108092+T3KScarecrow@users.noreply.github.com>
2026-04-02 20:59:01 +0800

    E

--- project_gehenna.py
+++ project_gehenna.py
@@ -1,3 +1,14 @@
 import sys
 import time
 import numpy as np
+import renderer.py as R
+
+
+text = "Initialising"
+R.Typer(text)
+R.LoadingDots()
+text = "\nInitialisation complete."
+R.Typer(text)
+text = "\nBooting Project Gehenna"
+R.Typer(text)
+R.LoadingDots()
